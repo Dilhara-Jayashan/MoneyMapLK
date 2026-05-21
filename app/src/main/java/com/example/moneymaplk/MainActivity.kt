a68a27f75b9ea1454d35b7bc03e9128bdde0ba8e
Dilhara-Jayashan <140555849+Dilhara-Jayashan@users.noreply.github.com>
2026-05-21 19:03:09 +0530

    create

--- app/src/main/java/com/example/moneymaplk/MainActivity.kt
+++ app/src/main/java/com/example/moneymaplk/MainActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.moneymaplk.core.navigation.MoneyMapNavGraph
-import com.example.moneymaplk.core.theme.MoneyMapLKTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
